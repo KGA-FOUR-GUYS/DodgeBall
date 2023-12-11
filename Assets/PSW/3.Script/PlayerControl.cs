31d24ba76c8f1b9dab80067c9e0aef2ceb7f3286
swp1207 <swp1207@gmail.com>
2023-12-11 11:42:27 +0900

    [update] player UI
    UI합본
    [done]

--- Assets/PSW/3.Script/PlayerControl.cs
+++ Assets/PSW/3.Script/PlayerControl.cs
@@ -3,9 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using Mirror;
+using NetworkRoom;
 
+[RequireComponent(typeof(LocalDataManager))]
+[RequireComponent(typeof(GlobalDataManager))]
 public class PlayerControl : NetworkBehaviour
 {
+    // Players List to manage playerNumber
+    static readonly List<PlayerControl> playersList = new List<PlayerControl>();
+
+    private GlobalDataManager _globalDataManager;
+    private LocalDataManager _localDataManager;
+
+    [Header("Player UI")]
+    public GameObject globalUIPrefab;
+    public GameObject localUIPrefab;
+
+    GameObject globalUIObject;
+    GameObject localUIObject;
+    GlobalUIManager globalUIManager;
+    LocalUIManager localUIManager;
+
     [Header("Components")]
     public NavMeshAgent agent;
     public Animator animator;
@@ -34,6 +52,35 @@ public class PlayerControl : NetworkBehaviour
     {
         Speed = moveSpeed;
         ghostEffect = GetComponent<GhostEffect>();
+    }
+
+    private void Start()
+    {
+        TryGetComponent(out _globalDataManager);
+        TryGetComponent(out _localDataManager);
+
+        // Add this to the static Players List
+        playersList.Add(this);
+
+        CreateLocalUI();
+        CreateGlobalUI();
+        CanvasUIManager.SetActive(true);
+
+        // set the Player Color SyncVar
+        _localDataManager.color = Random.ColorHSV(0f, 1f, 0.9f, 0.9f, 1f, 1f);
+
+        // set the initial player data
+        _localDataManager.hitCount = 0;
+        _localDataManager.attackCount = 0;
+    }
+
+    private void OnDestroy()
+    {
+        playersList.Remove(this);
+
+        CanvasUIManager.SetActive(false);
+        DestroyLocalUI();
+        DestroyGlobalUI();
     }
 
     private void Update()
@@ -195,5 +242,50 @@ public class PlayerControl : NetworkBehaviour
     private void AniSpeed(float speed)
     {
         animator.speed = speed;
+    }
+
+    [ClientCallback]
+    private void CreateLocalUI()
+    {
+        _localDataManager.name = SQLManager.Instance.userInfo.ID;
+
+        localUIObject = Instantiate(localUIPrefab, CanvasUIManager.GetPlayersPanel());
+        localUIManager = localUIObject.GetComponent<LocalUIManager>();
+
+        _localDataManager.OnColorChanged += localUIManager.OnPlayerColorChanged;
+        _localDataManager.OnNameChanged += localUIManager.OnPlayerNameChanged;
+        _localDataManager.OnHitCountChanged += localUIManager.OnPlayerHitCountChanged;
+        _localDataManager.OnAttackCountChanged += localUIManager.OnPlayerAttackCountChanged;
+        _localDataManager.InvokeAll();
+    }
+
+    [ClientCallback]
+    private void DestroyLocalUI()
+    {
+        _localDataManager.OnColorChanged -= localUIManager.OnPlayerColorChanged;
+        _localDataManager.OnNameChanged -= localUIManager.OnPlayerNameChanged;
+        _localDataManager.OnHitCountChanged -= localUIManager.OnPlayerHitCountChanged;
+        _localDataManager.OnAttackCountChanged -= localUIManager.OnPlayerAttackCountChanged;
+
+        Destroy(localUIObject);
+    }
+
+    private void CreateGlobalUI()
+    {
+        if (!isLocalPlayer) return;
+
+        globalUIObject = Instantiate(globalUIPrefab, CanvasUIManager.GetMainPanel());
+        globalUIManager = globalUIObject.GetComponent<GlobalUIManager>();
+
+        _globalDataManager.OnGameTimeChanged += globalUIManager.OnGlobalTimeValueChanged;
+    }
+
+    private void DestroyGlobalUI()
+    {
+        if (!isLocalPlayer) return;
+
+        _globalDataManager.OnGameTimeChanged -= globalUIManager.OnGlobalTimeValueChanged;
+
+        Destroy(globalUIObject);
     }
 }
